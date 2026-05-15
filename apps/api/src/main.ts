392afc70361060048e80c86e92c2ea3f962821ec
ThanhDang <thomas@ahaslides.com>
2026-05-15 23:33:19 +0700

    feat(api): enable CORS for localhost:3000 web dev

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
