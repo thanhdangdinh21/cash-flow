14bbd905e909891d41d8d0428eae974525b613bc
ThanhDang <thanh.dangdinh21@gmail.com>
2026-05-28 00:30:39 +0700

    feat(mobile): install i18n deps and extend Metro watchFolders

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+config.watchFolders = [
+  path.resolve(__dirname, '../../packages/i18n'),
+  path.resolve(__dirname, '../../packages/shared'),
+];
+
+module.exports = withNativeWind(config, { input: './global.css' });
